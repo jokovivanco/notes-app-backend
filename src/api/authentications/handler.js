39a1de016cb4b440467d67a8a0b6bb443b2cfd7a
jokovivanco <jokovivanco@gmail.com>
2022-11-21 15:36:42 +0800

    authorization implemented

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -61,9 +61,9 @@ class AuthenticationsHandler {
 
       const { refreshToken } = request.payload;
       await this._authenticationsService.verifyRefreshToken(refreshToken);
-      const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
+      const { id } = await this._tokenManager.verifyRefreshToken(refreshToken);
 
-      const accessToken = this._tokenManager.generateAccessToken({ id });
+      const accessToken = await this._tokenManager.generateAccessToken({ id });
       return {
         status: 'success',
         message: 'Access Token berhasil diperbarui',
